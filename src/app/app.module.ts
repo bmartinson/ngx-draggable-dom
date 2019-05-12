581375de66dbada18a067a4aa0e3cf00f66a05d8
bmartinson <brian@brianmartinson.com>
2019-05-12 00:35:18 -0700

    Better Examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { NgxDraggableDomModule } from "projects/ngx-draggable-dom/src/lib/ngx-draggable-dom.module";
+import { NgxDraggableDomModule } from "../../projects/ngx-draggable-dom/src/public_api";
 
 @NgModule({
   declarations: [
