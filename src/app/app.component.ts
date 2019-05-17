14335c8a5fb3132e05a663e12135a8c6bd8a2255
bmartinson <brian@brianmartinson.com>
2019-05-17 12:52:50 -0700

    Example Event Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ViewChildren, QueryList, ChangeDetectorRef, ChangeDetectionStrategy, ViewRef } from "@angular/core";
 import { NgxDraggableDomDirective } from "../../projects/ngx-draggable-dom/src/public_api";
+import { NgxDraggableBoundsCheckEvent } from "projects/ngx-draggable-dom/src/lib/classes/ngx-draggable-bounds-check-event";
+import { NgxDraggableMoveEvent } from "projects/ngx-draggable-dom/src/lib/classes/ngx-draggable-move-event";
 
 @Component({
   selector: "ngx-dd-root",
@@ -33,6 +35,26 @@ export class AppComponent implements AfterViewInit {
         el.reset();
       });
     }
+  }
+
+  /**
+   * Event handler that prints a move event's name and the data contained in the event.
+   *
+   * @param name The name of the event we are executing for.
+   * @param event The event to print data for.
+   */
+  public onPrintMoveEvent(name: string, event: NgxDraggableMoveEvent): void {
+    console.log(name, event.position);
+  }
+
+  /**
+   * Event handler that prints an edge event's name and the data contained in the event.
+   *
+   * @param name The name of the event we are executing for.
+   * @param event The event to print data for.
+   */
+  public onPrintEdgeEvent(name: string, event: NgxDraggableBoundsCheckEvent): void {
+    console.log(name, event);
   }
 
 }
