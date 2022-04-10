a662dd1f671948b149e7c90563ef061be09b616c
bmartinson <brian@brianmartinson.com>
2022-04-09 23:36:29 -0700

    Rename Sample Wrapper + Use Full Ivy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('ngx-draggable-dom-library app is running!');
+    expect(await page.getTitleText()).toEqual('ngx-draggable-dom-sample app is running!');
   });
 
   afterEach(async () => {
